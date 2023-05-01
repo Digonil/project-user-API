9e994d2e94b41522554c563f61eefe00df35cab0
Digonil <diego.niles@hotmail.com>
2023-05-01 20:15:35 -0300

    First commit

---
+++ src/test/java/com/niles/userapi/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niles.userapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
